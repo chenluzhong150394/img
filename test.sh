b6c8efc9e7c88f7e5440ca025aa8c4a0ec8f4fb3
chen <18438128833@163.com>
2019-08-03 22:04:59 +0800

    $currentTimeStamp

--- test.sh
+++ test.sh
@@ -6,6 +6,10 @@ timeStamp=`date -d "$current" +%s`
 #将current转换为时间戳，精确到毫秒  
 currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000)) 
 
+dir_path='/home/chen/图片/git/'
+
+cd $dir_path
+
 echo $currentTimeStamp
 addstr=`git add .`
 commitstr=`git commit -m '$currentTimeStamp'`
